a1d89592031e8a89bb765d1cd3a0a535cae9ebb9
Albert Augusto <augustothelema@gmail.com>
2024-05-10 20:57:19 -0300

    ok

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ function playRockPaperScissor (player1,player2){
 
     if (player1 == pedra && player2 == pedra || player1 == tesoura && player2 == tesoura || player1 == papel && player2 == papel){
         return `Empate!`;
-    }   else if (player1 == pedra && player2 == tesoura || player1 == papel && player2 == pedra || player1 == tesoura && player2 == papel){
-            return `Jogador 1 venceu!`;
-    }   else if (player1 == pedra && player2 == papel || player1 == tesoura && player2 == pedra || player1 == papel && player2 == tesoura){
-            return `Jogador 2 venceu!`;
+    }else if (player1 == pedra && player2 == tesoura || player1 == papel && player2 == pedra || player1 == tesoura && player2 == papel){
+        return `Jogador 1 venceu!`;
+    }else if (player1 == pedra && player2 == papel || player1 == tesoura && player2 == pedra || player1 == papel && player2 == tesoura){
+        return `Jogador 2 venceu!`;
     }
 }
 
